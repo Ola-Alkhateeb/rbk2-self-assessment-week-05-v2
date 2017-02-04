c033d0eb1e0569304fe6383e2c85929320845da8
Ola-Alkhateeb <ola.93.alkhateeb@gmail.com>
2017-02-04 08:54:37 +0200

    fizz & fozz solved

--- fizzbuzz-fozzbazz/app.js
+++ fizzbuzz-fozzbazz/app.js
@@ -1,23 +1,66 @@
 angular.module('app', ['ngRoute'])
-  .config(function($routeProvider) {
-    $routeProvider
-      .when('/fizzbuzz', {
-        templateUrl: 'partials/fizzbuzz.html',
-        controller: 'fizzbuzzCtrl'
-      })
-      .when('/fozzbazz', {
-        templateUrl: 'partials/fozzbazz.html',
-        controller: 'fozzbazzCtrl'
-      })
-      .otherwise({
-        redirectTo: '/fizzbuzz'
-      });
+.config(function($routeProvider) {
+  $routeProvider
+  .when('/fizzbuzz', {
+    templateUrl: 'partials/fizzbuzz.html',
+    controller: 'fizzbuzzCtrl'
   })
+  .when('/fozzbazz', {
+    templateUrl: 'partials/fozzbazz.html',
+    controller: 'fozzbazzCtrl'
+  })
+  .otherwise({
+    redirectTo: '/fizzbuzz'
+  });
+})
 // DO NOT MODIFY CODE ABOVE THIS LINE
 
 /*  HINT: Make sure your controllers, methods, and variables 
-    are named what $routeProvider and the partials are expecting  */
+are named what $routeProvider and the partials are expecting  */
 
-  .factory('counter', function(){})
-  .controller('fizzbuzzCtrl', function(){})
-  .controller('fozzbazzCtrl', function(){});
+.factory('counter', function(){
+  var cnt = 0 ;
+  var fizz = function(cnt){
+    if(cnt%3 === 0 && cnt%5===0){
+      return "fizzBuzz"
+    }else if(cnt%3 === 0){
+      return 'fizz'
+    }else if(cnt%5 === 0){
+      return 'Buzz'
+    }else {
+      return cnt ;
+    }
+  };
+
+  var fozz = function(cnt){
+   if(cnt%4 === 0 && cnt%6===0){
+    return "fozzBazz"
+  }else if(cnt%4 === 0){
+    return 'fozz'
+  }else if(cnt%6 === 0){
+    return 'Bazz'
+  }else {
+    return cnt ;
+  }
+
+}
+return {
+  cnt:cnt,
+ fizz:fizz,
+ fozz:fozz 
+}
+})
+.controller('fizzbuzzCtrl', function($scope , counter){
+    $scope.display = 'start clicking';
+    $scope.increment = function(){
+        counter.cnt ++ 
+        $scope.display = counter.fizz(counter.cnt)
+    } 
+})
+.controller('fozzbazzCtrl', function($scope , counter){
+   $scope.display = 'start clicking';
+   $scope.increment = function(){
+       counter.cnt ++ 
+      $scope.display = counter.fozz( counter.cnt ++ )
+  } 
+});
